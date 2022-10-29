0485a3284f8772dff09e10cda5dec1d15fa547ff
ryukyeongseo <rks85227@gmail.com>
2022-10-29 15:57:31 +0900

    pt1

--- portfolio.js
+++ portfolio.js
@@ -92,3 +92,50 @@ gallery.addEventListener("touchend", (e) => {
         }
     }
 });
+
+//floating
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+    // gsap.to(요소, 시간, 옵션)
+    gsap.to(selector, random(1.5, 2.5), {
+        y: size,
+        repeat: -1, // -1 무한반복
+        yoyo: true, // 애니메이션 되돌아오기(설정안할 시 끊김)
+        ease: Power1.easeInOut, // 타이밍함수
+        delay: random(0, delay) // 지연시간
+    })
+}
+floatingObject('.intro_star1', 1, 15)
+floatingObject('.intro_star2', .5, 15)
+floatingObject('.intro_star3', 1.5, 20)
+
+//reveal section
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target); //no more observing after postions read
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+    rootMargin: '200px',
+});
+
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
+
